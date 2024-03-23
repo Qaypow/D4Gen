1394d50f73d3f7fb413ccca5965b9a2d3af88371
pfarret81 <pierre.farret81@gmail.com>
2024-03-23 15:49:35 +0100

    Ajout partie serveur

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     host: 'localhost',
-    user: 'root', // Utilisateur root
-    password: 'LeafAId4gen', // Mot de passe root
+    user: 'root',
+    password: 'LeafAId4gen'
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,9 @@
-const express = require('express');
 const mysql = require('mysql2');
 const config = require('./config');
+const express = require('express');
 
 const app = express();
 const port = 3000;
-
-// Middleware pour parser le JSON
-app.use(express.json());
-
-// Middleware pour les logs
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
 
 const connection = mysql.createConnection({
     ...config,
@@ -29,19 +20,19 @@ connection.connect((err) => {
     console.log('Connexion à la base de données MySQL établie.');
 });
 
-// Définir une route POST pour ajouter une ligne à la base de données
-app.post('/api/addData', (req, res) => {
-    const { data } = req.body; // Supposons que vous recevez les données à ajouter dans le corps de la requête POST
-    // Insérer une nouvelle ligne dans la base de données
-    connection.query('INSERT INTO ma_table (colonne) VALUES (?)', [data], (err, result) => {
-        if (err) {
-            console.error('Erreur lors de l\'insertion des données dans la base de données : ', err);
-            res.status(500).json({ error: 'Erreur lors de l\'insertion des données' });
-            return;
-        }
-        console.log('Données insérées avec succès dans la base de données.');
-        res.json({ message: 'Données insérées avec succès dans la base de données.' });
-    });
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Middleware pour les logs
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// Définir les routes de votre API
+app.get('/api/endpoint', (req, res) => {
+    // Logique pour gérer la requête
+    res.json({ message: 'Réponse de l\'API' });
 });
 
 // Écouter les requêtes sur le port spécifié
